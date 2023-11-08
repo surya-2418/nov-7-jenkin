7f6d527fbf4e5dd45dcfd40f4f906ceb59ba2ad3
surya <surya@gmail.com>
2023-11-08 11:27:52 +0000

    script file

---
+++ java.sh
@@ -0,0 +1,16 @@
+#! bin/bash
+
+javac five.java
+java five.java
+
+javac four.java
+java four.java
+
+javac one.java
+java one.java
+
+javac three.java
+java three.java
+
+javac two.java
+java two.java
